f86ced212e9cf213eac5520258ef4a48889acd36
Stanislav Petrov <sv.petrov@kodep.pro>
2019-06-16 02:22:54 +0300

    improve package card and add update button

--- Objects/Objects.cs
+++ Objects/Objects.cs
@@ -6,10 +6,16 @@ namespace WhiteUnitySpa.Objects
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string FullName { get; set; }
         public string ProjectUrl { get; set; }
         public string UrlForManifest { get; set; }
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+        public string[] Dependencies { get; set; }
+        public string Category { get; set; }
+        public string Unity { get; set; }
     }
+
 
     public class PagingResultBaseDto
     {
